5dfa892a379f662439e06a1746e33f7319c6cee5
buiviet666 <loverbee01@gmail.com>
2023-10-13 17:54:40 +0700

    update the res page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// click navbar while small device
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const closebtn = document.getElementById("close");
@@ -12,4 +13,25 @@ if (closebtn) {
     closebtn.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+
+// ==========================================================
+// click small img to main img ("single product")
+let main_img = document.getElementById("main-img");
+let small_img = document.getElementsByClassName("small-img");
+
+small_img[0].onclick = function () {
+    main_img.src = small_img[0].src;
+}
+
+small_img[1].onclick = function () {
+    main_img.src = small_img[1].src;
+}
+
+small_img[2].onclick = function () {
+    main_img.src = small_img[2].src;
+}
+
+small_img[3].onclick = function () {
+    main_img.src = small_img[3].src;
 }
